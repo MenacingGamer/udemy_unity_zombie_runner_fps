799e09044722993ac6841b7010bc39d0fd7a1493
DESKTOP-5Q2KHUR\Dennis <dennismenace82484@gmail.com>
2017-09-23 16:09:39 -0700

    helicopter set up.
    Helicopter is made and sounds rigged up.

---
+++ Assets/Scripts/Heliacopter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heliacopter : MonoBehaviour {
+	public AudioClip helicopterinbound;
+
+	private bool called = false;
+	private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("CallHeli") && !called) {
+			called = true;
+			audioSource.clip =  helicopterinbound;
+			audioSource.Play();
+		}
+	}
+}
