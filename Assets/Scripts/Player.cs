72badfe8bc510021b65220a6a110c4ea8ab35182
DESKTOP-5Q2KHUR\Dennis <dennismenace82484@gmail.com>
2017-09-23 12:24:49 -0700

    spwanPoints
    Spawn points made and player script started

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Transform playerSpawnPoints;
+	public bool reSpawn = false;
+
+	private bool lastToggle = false;
+	private Transform[] spawnPoints;
+	// Use this for initialization
+	void Start () {
+	spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform> ();
+	print (spawnPoints.Length);
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (lastToggle != reSpawn) {
+			Respawn ();
+			reSpawn = true;
+		} else {
+			lastToggle = reSpawn;
+		}
+	}
+	private void Respawn ()
+	{
+	int i = Random.Range (1, spawnPoints.Length);
+	transform.position = spawnPoints [i].transform.position;
+	}
+}
